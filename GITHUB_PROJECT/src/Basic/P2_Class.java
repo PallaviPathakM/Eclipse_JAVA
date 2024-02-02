71b3c6de83c86d6c0ddb86ce99e38c1842cd555e
erami <erami@host.docker.internal>
2024-02-02 19:30:02 +0000

    SYSO lines for class

--- GITHUB_PROJECT/src/Basic/P2_Class.java
+++ GITHUB_PROJECT/src/Basic/P2_Class.java
@@ -4,6 +4,7 @@ public class P2_Class {
 
 	public static void main(String[] args) {
 		System.out.println("Second class for Github repositry");
+		System.out.println("New SYSO lines for class");
 
 	}
 
